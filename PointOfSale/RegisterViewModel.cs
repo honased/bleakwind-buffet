67fa7af45fb10ecbd46de31bb79b9d5519dc9de6
Eric Honas <honased@gmail.com>
2020-10-13 13:21:10 -0500

    Began preparation for POS Milestone 4

---
+++ PointOfSale/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RoundRegister;
+
+namespace PointOfSale
+{
+    public class RegisterViewModel
+    {
+        public RegisterViewModel()
+        {
+            
+        }
+    }
+}
---
+++ PointOfSale/Screens/PaymentOptionsComponent.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale.Screens
+{
+    /// <summary>
+    /// Interaction logic for PaymentOptionsComponent.xaml
+    /// </summary>
+    public partial class PaymentOptionsComponent : UserControl
+    {
+        public PaymentOptionsComponent()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
