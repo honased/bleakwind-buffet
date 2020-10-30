5cd6ade732e94b449cfde0a9b6d27b8197c69490
Eric Honas <honased@gmail.com>
2020-10-30 09:44:52 -0500

    The index page now satisfies the requirements.

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BleakwindBuffet.Data.Classes;
+using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Enums;
+using BleakwindBuffet.Data.Sides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -20,6 +25,50 @@ namespace Website.Pages
         public void OnGet()
         {
 
+        }
+
+        public IEnumerable<Entree> Entrees
+        {
+            get
+            {
+                foreach(Entree entree in Menu.Entrees())
+                {
+                    yield return entree;
+                }
+            }
+        }
+
+        public IEnumerable<Drink> Drinks
+        {
+            get
+            {
+                foreach (Drink drink in Menu.Drinks())
+                {
+                    yield return drink;
+                }
+            }
+        }
+
+        public IEnumerable<Side> Sides
+        {
+            get
+            {
+                foreach (Side side in Menu.Sides())
+                {
+                    yield return side;
+                }
+            }
+        }
+
+        public IEnumerable<string> SodaFlavors
+        {
+            get
+            {
+                foreach(string s in Enum.GetNames(typeof(SodaFlavor)))
+                {
+                    yield return s;
+                }
+            }
         }
     }
 }
